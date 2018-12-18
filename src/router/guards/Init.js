d05e322248e988a5cdf3ebaaaf30d1834d68fe36
Eugene Zubakhin <newzubakhin@gmail.com>
2018-12-18 19:34:55 +0300

    Cleaning code

--- src/router/guards/Init.js
+++ src/router/guards/Init.js
@@ -2,14 +2,12 @@ import store from '../../store/store'
 
 export default (to, from, next) => {
   const accessToken = localStorage.getItem('accessToken')
-  console.log(accessToken)
 
   if (!accessToken) {
     return next()
   }
 
   store.commit('auth/setAccessToken', accessToken)
-  console.log('Установлен access токен')
 
   store.dispatch('auth/authenticate')
     .then(() => {

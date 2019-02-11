68abf7bb9bf33734ad3d491d9e7eb9f80c7e83f5
mpetrov <petrovma92@gmail.com>
2019-02-11 19:32:25 +0300

    Add Axios

--- src/main.js
+++ src/main.js
@@ -30,7 +30,7 @@ Vue.http.interceptors.push((request, next) => {
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   methods: {
     setAccessKey (newJwt) {
       Vue.http.headers.common['Authorization'] = 'Bearer ' + newJwt
@@ -45,3 +45,35 @@ new Vue({
   },
   template: '<App v-on:jwtUpdated="setAccessKey" v-on:logout="logout" />'
 })
+const axios = require('axios')
+
+// Make a request for a user with a given ID
+axios.get('/*')
+  .then(function (response) {
+    // handle success
+    console.log('response')
+    console.log(response)
+  })
+  .catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      console.log('The request was made and the server responded with a status code')
+      // that falls out of the range of 2xx
+      console.log(error.response.data)
+      console.log(error.response.status)
+      console.log(error.response.headers)
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.log('The request was made but no response was received')
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      console.log(error.request)
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      console.log('Something happened in setting up the request that triggered an Error')
+      console.log('Error', error.message)
+    }
+  })
+  .then(function () {
+    // always executed
+  })
